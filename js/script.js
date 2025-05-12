0100b3e7ad1335057f7f08127b078936548e5ad9
Oleg <oleg.pancheshniy@gmail.com>
2025-05-12 09:02:56 +0300

    bug fix practic_1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 /* Задание на урок:
 
 1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
@@ -22,23 +21,22 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
-'use strict';
+"use strict";
 
 const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
 
 const personalMovieDB = {
-    count = numberOfFilms,
-    movies = {},
-    actors = {},
-    genres =[],
-    privat = false
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', '');
-const b = prompt('На сколько оцените его?', '');
-const c = prompt('Один из последних просмотренных фильмов?', '');
-const d = prompt('На сколько оцените его?', '');
-
+const a = prompt("Один из последних просмотренных фильмов?", "");
+const b = prompt("На сколько оцените его?", "");
+const c = prompt("Один из последних просмотренных фильмов?", "");
+const d = prompt("На сколько оцените его?", "");
 
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
