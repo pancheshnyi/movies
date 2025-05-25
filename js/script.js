211f248bb34d77ca0fd1000d414a897311ac3cd5
Oleg <oleg.pancheshniy@gmail.com>
2025-05-25 21:10:18 +0300

    dz1

--- js/script.js
+++ js/script.js
@@ -23,22 +23,22 @@
 
 // --------------------------------------------------------------------
 
-// "use strict";
+"use strict";
 
-// const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
 
-// const personalMovieDB = {
-//   count: numberOfFilms,
-//   movies: {},
-//   actors: {},
-//   genres: [],
-//   privat: false,
-// };
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
 
-// const a = prompt("Один из последних просмотренных фильмов?", "");
-// const b = prompt("На сколько оцените его?", "");
-// const c = prompt("Один из последних просмотренных фильмов?", "");
-// const d = prompt("На сколько оцените его?", "");
+const a = prompt("Один из последних просмотренных фильмов?", "");
+const b = prompt("На сколько оцените его?", "");
+const c = prompt("Один из последних просмотренных фильмов?", "");
+const d = prompt("На сколько оцените его?", "");
 
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
